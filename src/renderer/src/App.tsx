13d5b08b6b920082aa4cf588cceaaf5fd6a8a427
Majkipl27 <tomaszmamala@gmail.com>
2023-11-20 20:09:13 +0100

    Initialized electron + react + ts, initialized react router and tailwindcss

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,24 @@
+import { useLocation, useRoutes } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+import { cloneElement } from 'react'
+
+function App(): JSX.Element | null {
+  const element = useRoutes([
+    {
+      path: '/',
+      element: <h1>Home</h1>
+    },
+  ])
+
+  const location = useLocation()
+
+  if (!element) return null
+
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      {cloneElement(element, { key: location.pathname })}
+    </AnimatePresence>
+  )
+}
+
+export default App
