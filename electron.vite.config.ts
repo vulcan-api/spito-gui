e664070000b33bf6f815a8073b7d3c4a386ec46f
maxidragon <maksymilian.gala1@gmail.com>
2024-01-12 16:34:52 +0100

    Fix build

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,8 +2,40 @@ import { resolve } from "path";
 import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 import react from "@vitejs/plugin-react";
 
+const workerImportMetaUrlRE =
+  /\bnew\s+(?:Worker|SharedWorker)\s*\(\s*(new\s+URL\s*\(\s*('[^']+'|"[^"]+"|`[^`]+`)\s*,\s*import\.meta\.url\s*\))/g;
+
 export default defineConfig({
   main: {
+    worker: {
+      format: "es",
+      plugins: [
+        {
+          name: "foo",
+          enforce: "pre",
+          transform(code): string | undefined {
+            if (
+              code.includes("new Worker") &&
+              code.includes("new URL") &&
+              code.includes("import.meta.url")
+            ) {
+              const result = code.replace(
+                workerImportMetaUrlRE,
+                `((() => { throw new Error('Nested workers are disabled') })()`
+              );
+              return result;
+            }
+            return undefined;
+          }
+        }
+      ],
+      rollupOptions: {
+        output: {
+          chunkFileNames: "assets/worker/[name]-[hash].js",
+          assetFileNames: "assets/worker/[name]-[hash].js"
+        }
+      }
+    },
     plugins: [externalizeDepsPlugin()]
   },
   preload: {
