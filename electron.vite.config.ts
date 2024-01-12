0a59210a7adddd827ee0ebdfd10d3ed431bc6751
maxidragon <maksymilian.gala1@gmail.com>
2024-01-12 16:52:42 +0100

    Trying to fix build

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,38 +2,11 @@ import { resolve } from "path";
 import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 import react from "@vitejs/plugin-react";
 
-const workerImportMetaUrlRE =
-  /\bnew\s+(?:Worker|SharedWorker)\s*\(\s*(new\s+URL\s*\(\s*('[^']+'|"[^"]+"|`[^`]+`)\s*,\s*import\.meta\.url\s*\))/g;
-
 export default defineConfig({
   main: {
-    worker: {
-      format: "es",
-      plugins: [
-        {
-          name: "foo",
-          enforce: "pre",
-          transform(code): string | undefined {
-            if (
-              code.includes("new Worker") &&
-              code.includes("new URL") &&
-              code.includes("import.meta.url")
-            ) {
-              const result = code.replace(
-                workerImportMetaUrlRE,
-                `((() => { throw new Error('Nested workers are disabled') })()`
-              );
-              return result;
-            }
-            return undefined;
-          }
-        }
-      ],
+    build: {
       rollupOptions: {
-        output: {
-          chunkFileNames: "assets/worker/[name]-[hash].js",
-          assetFileNames: "assets/worker/[name]-[hash].js"
-        }
+        external: ["core-js-pure/stable/object/assign.js"]
       }
     },
     plugins: [externalizeDepsPlugin()]
