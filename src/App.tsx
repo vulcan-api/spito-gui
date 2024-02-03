71dce7b13e5c9e3a3fd99a471663b89842dc30f7
cooligus <pewienjaaa@gmail.com>
2024-02-03 19:07:50 +0100

    Return good old App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,9 @@ import Settings from "./Pages/Settings/Settings";
 import EnvironmentPage from "./Pages/Environment/EnvironmentPage";
 import Home from "./Pages/Home/Home";
 import SavedEnvironments from "./Pages/SavedEnvironments/SavedEnvironments";
-import { cloneElement, useState } from "react";
-import { listen, Event } from "@tauri-apps/api/event";
-import { invoke } from "@tauri-apps/api/tauri";
+import { cloneElement } from "react";
 
 function App(): JSX.Element | null {
-    const [log, setLog] = useState("");
     const element = useRoutes([
         {
             path: "/",
@@ -50,40 +47,10 @@ function App(): JSX.Element | null {
     const location = useLocation();
     if (!element) return null;
 
-    // TODO: move it
-    listen("Echo", (event: Event<{ mess_type: string; message: string }>) => {
-        setLog(
-            "[" + event.payload.mess_type + "]" + " " + event.payload.message
-        );
-    });
-
     return (
         <>
             <AnimatePresence mode="wait" initial={false}>
                 <Toaster />
-                {/*TODO: remove it*/}
-                <div
-                    style={{
-                        position: "absolute",
-                        background: "white",
-                        color: "black",
-                        top: 0,
-                        left: 0,
-                    }}
-                >
-                    <button
-                        onClick={() => {
-                            invoke("start_spito_cli", {
-                                ruleset: "avorty/spito-ruleset",
-                                rule: "info",
-                            });
-                        }}
-                    >
-                        Run spito
-                    </button>
-                    {log}
-                </div>
-                {/**/}
                 {cloneElement(element, { key: location.pathname })}
             </AnimatePresence>
         </>
