6aa3d8845b171fb62b3c73e31b5df6f852ea69a2
Maksymilian Gala <maksymilian.gala1@gmail.com>
2024-02-25 16:05:27 +0100

    turn off eslint overlay

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
-import eslint from "vite-plugin-eslint";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        eslint(),
         checker({
             overlay: { initialIsOpen: false },
             typescript: true,
